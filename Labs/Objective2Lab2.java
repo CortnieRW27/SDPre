5bf6e7a61e17c828b402da97849e86c34f87e24e
Cortnie Barrow <mommy24227@gmail.com>
2023-05-08 10:36:20 -0400

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Cortnie Wirtanen";
+		int age = 36;
+		String state = "Virginia";
+		System.out.println("Name: Cortnie Wirtanen");
+		System.out.println("Age: 36");
+		System.out.println("State: Virginia");
+	}
+}
