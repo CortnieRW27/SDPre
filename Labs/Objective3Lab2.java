4f6b9cc308c6841d44779c06b13eaac8c8384ce0
Cortnie Barrow <mommy24227@gmail.com>
2023-05-08 12:38:50 -0400

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		double side1 = (7 *7);
+		double side2 = (10 *10);
+		double hypotenuse = Math.sqrt(side1 + side2);
+
+		System.out.println("The hypotenuse of a trinagle with sides 7.0 and 10.0 is " + " " + hypotenuse);
+	}
+}
