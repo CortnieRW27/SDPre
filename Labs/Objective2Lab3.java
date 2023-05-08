6c93f728b136e0d586bb50f6d9fdaaab729cd768
Cortnie Barrow <mommy24227@gmail.com>
2023-05-08 11:16:40 -0400

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int year = 1987;
+		String month = "February";
+		boolean weekend = false;
+		System.out.println("1987");
+		System.out.println("February");
+		System.out.println("false");
+	}
+}
