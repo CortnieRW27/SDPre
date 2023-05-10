580b9f772df8c4f94ef1b8493658fcfe3003aa4e
Cortnie Barrow <mommy24227@gmail.com>
2023-05-10 10:07:04 -0400

    complete objective7lab2

--- Labs/Objective7Lab2.java
+++ Labs/Objective7Lab2.java
@@ -1,10 +1,12 @@
 public class Objective7Lab2 {
   public static void main(String[] args) {
-  int counter = 0;
+
+    int counter = 0;
 
     while(counter <= 20) {
       System.out.println(+ counter);
       counter = counter + 1;
+
     }
   }
 }
